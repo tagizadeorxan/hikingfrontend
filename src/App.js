77e24f99ac4d9e309e601693cbbe7e1037a8da4e
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2020-10-26 21:55:09 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Trails from './Trails';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Trail from './Trails/eachTrail.js';
 import Header from './Header';
