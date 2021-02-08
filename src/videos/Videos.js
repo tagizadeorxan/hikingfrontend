2ebdfd01129b52eb397eea2e7461929d4aa7211d
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2021-02-08 15:27:34 +0400

    iframes title add

--- src/videos/Videos.js
+++ src/videos/Videos.js
@@ -4,11 +4,11 @@ import './videos.css'
 const Videos = () => {
     return (
         <div className="videos-frames">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/N8LpLSEEBO4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/5QhvcKAV6ZY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/KaXFEdS1yys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/nltWJiCLwoY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ySSOwbNmnWg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" title="enbil lake" height="315" src="https://www.youtube.com/embed/N8LpLSEEBO4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" title="wedding" height="315" src="https://www.youtube.com/embed/5QhvcKAV6ZY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" title="hirkan park" height="315" src="https://www.youtube.com/embed/KaXFEdS1yys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" title="niyal mountain" height="315" src="https://www.youtube.com/embed/nltWJiCLwoY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" title="qax ilisu" height="315" src="https://www.youtube.com/embed/ySSOwbNmnWg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             
         </div>
     )
