f983d32f59a8df20d0143aa428cb57138e069aed
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2021-03-29 20:47:00 +0400

    videos from database

--- src/videos/Videos.js
+++ src/videos/Videos.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './videos.css'
 
 const Videos = () => {
+
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        fetch('https://hikings.herokuapp.com/api/data/hiking/videos').then(res => res.json()).then(videos => setVideos(videos))
+    }, [videos])
     return (
         <div className="videos-frames">
-            <iframe width="560" title="enbil lake" height="315" src="https://www.youtube.com/embed/N8LpLSEEBO4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" title="wedding" height="315" src="https://www.youtube.com/embed/5QhvcKAV6ZY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" title="hirkan park" height="315" src="https://www.youtube.com/embed/KaXFEdS1yys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" title="niyal mountain" height="315" src="https://www.youtube.com/embed/nltWJiCLwoY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" title="qax ilisu" height="315" src="https://www.youtube.com/embed/ySSOwbNmnWg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            
+            { videos.length ? videos.map((video, i) =>
+                <iframe width="560" title={video.title} height="315" src={video.embed_url} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" />
+            ) : null}
         </div>
     )
 }
